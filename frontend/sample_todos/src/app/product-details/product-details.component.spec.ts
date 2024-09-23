09ab00d542b859a8dcb0c2fbc305edaf4318b821
yamao-sys <yamaopanku@gmail.com>
2024-09-23 16:15:57 +0900

    テストにproviderRouterを追加

--- frontend/sample_todos/src/app/product-details/product-details.component.spec.ts
+++ frontend/sample_todos/src/app/product-details/product-details.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProductDetailsComponent } from './product-details.component';
+import { provideRouter } from '@angular/router';
 
 describe('ProductDetailsComponent', () => {
   let component: ProductDetailsComponent;
@@ -8,9 +9,9 @@ describe('ProductDetailsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ProductDetailsComponent]
-    })
-    .compileComponents();
+      imports: [ProductDetailsComponent],
+      providers: [provideRouter([])],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ProductDetailsComponent);
     component = fixture.componentInstance;
