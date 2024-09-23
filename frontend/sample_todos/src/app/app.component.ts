ee1bf5c347904b5ad06f5f6de8029d69b75f28a4
yamao-sys <yamaopanku@gmail.com>
2024-09-23 12:13:15 +0900

    ngFor, ngIf

--- frontend/sample_todos/src/app/app.component.ts
+++ frontend/sample_todos/src/app/app.component.ts
@@ -1,13 +1,27 @@
+import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgFor, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'sample_todos';
+
+  products = [
+    {
+      name: 'test name 1',
+      price: 100,
+      description: 'test description 1',
+    },
+    {
+      name: 'test name 2',
+      price: 200,
+      description: '',
+    },
+  ];
 }
