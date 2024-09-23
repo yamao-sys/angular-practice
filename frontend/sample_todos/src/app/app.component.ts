96ac6ae10f47249feba43568645d53522b5da636
yamao-sys <yamaopanku@gmail.com>
2024-09-23 16:02:57 +0900

    商品詳細と共通で使用するダミーデータを外に切り出す

--- frontend/sample_todos/src/app/app.component.ts
+++ frontend/sample_todos/src/app/app.component.ts
@@ -1,30 +1,21 @@
 import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { Product } from './interface/product';
+import { products } from './data/products';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgFor, NgIf, ProductAlertsComponent],
+  imports: [RouterOutlet, NgFor, NgIf, ProductAlertsComponent, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'sample_todos';
 
-  products = [
-    {
-      name: 'test name 1',
-      price: 100,
-      description: 'test description 1',
-    },
-    {
-      name: 'test name 2',
-      price: 200,
-      description: '',
-    },
-  ];
+  products: Product[] = products;
 
   share() {
     window.alert('share!!');
